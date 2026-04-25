7b4999391631dcdac9d92224327efbb94e922a60
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-04-25 16:35:27 +0600

    Re-Routed and Breaking News added

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Header from '@/components/Header';
-import Navbar from '@/components/Navbar';
-
+import Header from '@/components/shared/Header';
+import Navbar from '@/components/shared/Navbar';
+import BreakingNewsPage from '@/components/shared/BreakingNews';
 const MainLayout = ({children}) => {
   return (
     <>
       <Header />
+      <BreakingNewsPage></BreakingNewsPage>
       <Navbar></Navbar>
       {children}
     </>
---
+++ src/components/shared/BreakingNews.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Marquee from 'react-fast-marquee';
+const BreakingNewsPage = () => {
+  return (
+    <div>
+      <Marquee>
+        Lorem ipsum, dolor sit amet consectetur 
+        adipsicing elit. Deserunt aliquid 
+        repellendus o
+        dit repudiandae suscipit voluptate 
+        nesciunt at,
+        veritatis sed laudantium maiores fuga 
+        illum, sit atque fugiat architecto
+        amet accusamus itaque esse alias autem 
+        blanditiis eveniet! Labore distinctio 
+        ratione, cupiditate, porro quae
+        rat velit esse dolore voluptate
+        s voluptatibus deleniti mollitia aliquam
+         recusandae?
+      </Marquee>
+    </div>
+  );
+};
+
+export default BreakingNewsPage;
