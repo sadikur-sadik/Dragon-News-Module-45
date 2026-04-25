f7b601988b9bd32c8e90532b77afe3a688bb13ec
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-04-25 19:12:17 +0600

    Left and Right Side Design

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,9 +1,29 @@
+import LeftSide from "@/components/homepage/news/LeftSide";
+import RightSide from "@/components/homepage/RightSide";
+const getCategories = async () => {
 
+  const res = await fetch("https://openapi.programming-hero.com/api/news/categories",{next:{revalidate:5}});
+  return res.json();
+}
 
-export default function Home() {
+export default async function Home() {
+  const cate = await getCategories();
+  const categories = cate.data.news_category;
+  // console.log(categories)
+
+ 
+  
   return (
-   <div>
-    homePage
+   <div className="grid grid-cols-12 container mx-auto my-15 gap-2">
+    <div className="font-bold  col-span-3">
+      
+      <LeftSide categories={categories} activeId={null}></LeftSide>
+
+    </div>
+    <div className="font-bold text-3xl col-span-6 bg-purple-100">All News</div>
+    <div className=" col-span-3 ">
+      <RightSide/>
+    </div>
    </div>
   );
 }
---
+++ src/components/homepage/news/LeftSide.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Link from 'next/link';
+const LeftSide = ({ categories , activeId}) => {
+  return (
+    <>
+      <div className="text-xl border-b pb-2 border-gray-200">All Catergories</div>
+
+      <ul className="flex flex-col gap-4">
+      
+
+        {categories.map((category, i) => <li
+         key={i} 
+         className=
+         {`${activeId == category.category_id  && "bg-slate-100" } rounded-md  text-lg mt-5 font-semibold text-center `}>
+          <Link className='block p-2' href={`/category/${category.category_id}`}>{category.category_name}</Link></li>)}
+
+      </ul>
+    </>
+  );
+};
+
+export default LeftSide;
