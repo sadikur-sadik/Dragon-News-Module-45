902217281661fa2f416ebc0a9b77412b191932a0
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-04-25 16:55:44 +0600

    NavBar design

--- src/components/shared/BreakingNews.jsx
+++ src/components/shared/BreakingNews.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
+
+const news = [
+  {
+    "id": 1,
+    "news": "Nottingham Forest thrash Sunderland 5-0 to move eight points clear of the Premier League relegation zone."
+  },
+  {
+    "id": 2,
+    "news": "Manchester City and Southampton prepare for today's high-stakes FA Cup semi-final clash at Wembley."
+  },
+  {
+    "id": 3,
+    "news": "Roberto De Zerbi's Tottenham face critical pressure in the relegation battle ahead of their match against Wolves."
+  },
+  {
+    "id": 4,
+    "news": "Jake Slaughter and Austin Barber selected by the Chargers and Browns on Day 2 of the 2026 NFL Draft."
+  },
+  {
+    "id": 5,
+    "news": "Minnesota United FC puts their five-game winning streak on the line tonight against Son Heung-Min's LAFC."
+  }
+]
+
 const BreakingNewsPage = () => {
   return (
-    <div>
-      <Marquee>
-        Lorem ipsum, dolor sit amet consectetur 
-        adipsicing elit. Deserunt aliquid 
-        repellendus o
-        dit repudiandae suscipit voluptate 
-        nesciunt at,
-        veritatis sed laudantium maiores fuga 
-        illum, sit atque fugiat architecto
-        amet accusamus itaque esse alias autem 
-        blanditiis eveniet! Labore distinctio 
-        ratione, cupiditate, porro quae
-        rat velit esse dolore voluptate
-        s voluptatibus deleniti mollitia aliquam
-         recusandae?
+    <div className='mt-5 container mx-auto flex justify-between items-center gap-1 p-2 bg-gray-400'>
+      <button className='btn bg-[#d72050FF] p-4 text-white'>Latest News</button>
+      <Marquee pauseOnHover={true} >
+      {news.map((n,i) => <span className='ml-15' key={i}>{n.news}</span>)}
       </Marquee>
     </div>
   );
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import Link from "next/link";
+import useAvatar from "@/assets/user.png"
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className=" container mx-auto mt-3 flex justify-between items-center">
+      <div></div>
+      <ul className="flex gap-10">
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/career">Career</Link></li>
+      </ul>
+
+      <div className="flex items-center gap-5">
+        <Image src={useAvatar} alt="nav Logo"  width={45}  height={45}>
+
+        </Image>
+
+        <button className="btn bg-gray-900 text-white">
+          <Link href="/login" className="p-3">Login</Link>
+        </button>
+      </div>
     </div>
   );
 };
