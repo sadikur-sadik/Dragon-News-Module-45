b33a70bd868664fbb9162a01af294bd4dd9548c6
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-04-27 16:31:51 +0600

    Login and Register Design

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,42 @@
-// import React from 'react';
+"use client"
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
 
 const LoginPage = () => {
+  
+   const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+ 
+  const onSubmit = (data) => console.log(data)
   return (
-    <div>
-      
+    <div className="container mt-5 bg-slate-100 flex justify-center items-center mx-auto min-h-[80vh]">
+      <div className="rounded-xl bg-white p-4">
+        <h2 className="font-bold text-xl mb-5 text-center">Login Your Account</h2>
+
+         <form onSubmit={handleSubmit(onSubmit)}>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 space-y-4">
+            {/* <legend className="fieldset-legend">Login Your Account</legend> */}
+
+         
+            <label className="label">Email</label>
+            <input {...register("email")} type="email"  className="input" placeholder="Enter Email" />
+
+            <label className="label">Password</label>
+            <input {...register("password" , {required: "At least 4 digits of password is needed"})} type="password"  className="input" placeholder="Enter Password" />
+
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+        </form>
+        {errors.password && <p>{errors.password.message}</p>}
+        <p className="mt-4 text-sm text-gray-400">
+          Do not Have an Account? <Link className="text-red-500" href="/register">Register.</Link></p>
+      </div>
     </div>
   );
 };
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,9 +1,59 @@
-// import React from 'react';
+"use client"
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
 
 const RegisterPage = () => {
+  
+   const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+ 
+  const onSubmit = (data) =>{
+    
+    const {name , photo , email , password} = data;
+    console.log(name , photo , email , password)
+  }
   return (
-    <div>
-      
+    <div className="container mt-5 bg-slate-100 flex justify-center items-center mx-auto min-h-[80vh]">
+      <div className="rounded-xl bg-white p-4">
+        <h2 className="font-bold text-xl mb-5 text-center">Create Your Account</h2>
+
+         <form onSubmit={handleSubmit(onSubmit)}>
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 space-y-4">
+            {/* <legend className="fieldset-legend">Login Your Account</legend> */}
+
+            <label className="label">Name</label>
+            <input {...register("name", {required: "Name is required"})} type="text"  className="input" placeholder="Enter Name" />
+            
+
+             {errors.name && <p>{errors.name.message}</p>}
+
+            <label className="label">Photo URL</label>
+            <input {...register("photo", {required: "Photo URL required"})} type="text"  className="input" placeholder="Enter Photo URL" />
+            
+
+             {errors.photo && <p>{errors.photo.message}</p>}
+
+            <label className="label">Email</label>
+            <input {...register("email", {required: "Email is required"})} type="email"  className="input" placeholder="Enter Email" />
+
+             {errors.email && <p>{errors.email.message}</p>}
+
+            <label className="label">Password</label>
+            <input {...register("password" , {required: "At least 4 digits of password is needed"})} type="password"  className="input" placeholder="Enter Password" />
+            
+            {errors.password && <p>{errors.password.message}</p>}
+            <button className="btn btn-neutral mt-4">Login</button>
+          </fieldset>
+        </form>
+        
+     
+      </div>
     </div>
   );
 };
